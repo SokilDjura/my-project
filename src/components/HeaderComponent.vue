5dbfc997ea20308ddc9ea40c60b63d2dfa896041
Maksym <tsymbal12321@gmail.com>
2022-10-16 21:40:38 +0300

    Add header & sidebar

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,90 @@
+<template>
+  <header>
+    <div class='header-wrap d-flex'>
+      <div class='icon-wrap my-2 me-2'>
+        <b-icon class='me-2' font-scale='1.3' icon='gear-fill'/>
+        <b-icon class='mx-2' font-scale='1.3' icon='calendar2-check-fill'/>
+        <b-icon class='mx-2' font-scale='1.3' icon='bell-fill'/>
+        <b-icon class='mx-2' font-scale='1.3' icon='question-circle-fill'/>
+      </div>
+      <div class='w-50 me-auto'>
+        <b-input-group>
+          <b-input class='search-input' placeholder='Пошук'/>
+          <template #append>
+            <b-input-group-text>
+              <b-icon class='mx-2' font-scale='1.4' icon='search'/>
+            </b-input-group-text>
+          </template>
+        </b-input-group>
+      </div>
+      <b-button-group class='button-group-wrap'>
+        <b-button class='px-4' variant="primary">
+          <b-icon class='me-1' icon='card-checklist'/>
+          Послуга
+        </b-button>
+        <b-button class='header-btn-center px-4' variant="primary">
+          <b-icon class='me-1' icon='basket2-fill'/>
+          Товар
+        </b-button>
+        <b-button class='px-4' variant="primary">
+          <b-icon class='me-1' icon='calendar2-event'/>
+          Запись
+        </b-button>
+      </b-button-group>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent',
+}
+</script>
+
+<style scoped>
+header {
+  padding-left: 80px;
+  position: fixed;
+  width: 100%;
+}
+.header-wrap {
+  padding: 8px 12px;
+  border-bottom: 1px solid #ced4da;
+  box-shadow: 3px 3px 3px rgb(128 95 95 / 5%);
+}
+.icon-wrap svg {
+  color: #2c3e50;
+  opacity: .3;
+  transition: .3s;
+  cursor: pointer;
+}
+.icon-wrap svg:hover {
+  opacity: .5;
+}
+.search-input{
+  height: 38px;
+  border-right: 0;
+}
+.input-group-text{
+  border-left: 0;
+  border-radius: 0 0.375rem 0.375rem 0;
+  height: 38px;
+  background: transparent!important;
+  cursor: pointer;
+}
+.input-group-text svg{
+  color: #2c3e50;
+  opacity: .5;
+}
+.header-btn-center{
+  border-right: 2px solid #0062cb;
+  border-left: 2px solid #0062cb;
+}
+.button-group-wrap{
+  height: 38px;
+}
+.form-control:focus {
+   border-color: #ced4da !important;
+   box-shadow: none !important;
+}
+</style>
---
+++ src/components/SidebarComponent.vue
@@ -0,0 +1,103 @@
+<template>
+  <b-sidebar
+    backdrop-variant='dark'
+    no-header
+    :visible='true'
+    width='80px'
+    shadow
+  >
+    <b-button
+      v-b-modal.modal-1
+      class='mt-1 mb-2 px-1 py-1 mx-auto d-flex align-items-center button-geo'
+      variant="light"
+    ><b-icon icon='geo-alt-fill' font-scale='0.9' class='me-1'/>
+      <small>Головний</small>
+    </b-button>
+    <div class='mb-2 avatar-wrap'>
+      <b-avatar size='72px' src='https://placekitten.com/300/300' class='mb-2 mx-1'></b-avatar>
+      <div class='text-center'>Max</div>
+    </div>
+    <div class='text-center'>
+      <b-icon icon="box-arrow-right" font-scale="1.5" class='btn-exit'/>
+    </div>
+    <hr class='my-2'/>
+    <div v-for='item in sidebarButtons' :key='item.id'>
+      <b-button
+        variant="light"
+        class='px-1 my-1'
+      >
+        <b-icon :icon='item.icon' font-scale="1.5"></b-icon>
+        <small>{{ item.title }}</small>
+      </b-button>
+    </div>
+    <b-modal id='modal-1' title='BootstrapVue'>
+      <p class='my-4'>Hello from modal!</p>
+    </b-modal>
+  </b-sidebar>
+</template>
+
+<script>
+export default {
+  name: 'SideBar',
+  data () {
+    return{
+      sidebarButtons: [
+        {
+          id: 1,
+          title: 'Повістка',
+          icon: 'house-fill',
+          path: '#'
+        },
+        {
+          id: 2,
+          title: 'Відмітка',
+          icon: 'person-check-fill',
+          path: '#'
+        },
+        {
+          id: 3,
+          title: 'Росклад',
+          icon: 'calendar2-week-fill',
+          path: '#'
+        },
+        {
+          id: 4,
+          title: 'Таблиці',
+          icon: 'table',
+          path: '#'
+        },
+        {
+          id: 5,
+          title: 'Воронка',
+          icon: 'graph-up',
+          path: '#'
+        },
+        {
+          id: 5,
+          title: 'Аналітика',
+          icon: 'pie-chart-fill',
+          path: '#'
+        },
+        {
+          id: 5,
+          title: 'Модулі',
+          icon: 'speedometer',
+          path: '#'
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .button-geo{
+    font-size: 12px;
+  }
+  .avatar-wrap{
+    cursor: pointer;
+  }
+  .btn-exit{
+    cursor: pointer;
+  }
+</style>
